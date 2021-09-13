48efa7f25e2953bfb44c18d4fbe990e997f01d70
Ben Phung <benphung123@gmail.com>
2021-09-13 01:52:12 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,40 @@
+/*
+ * UCF COP3330 Fall 2021 Assignment 1 Solution
+ * Copyright 2021 Ben Phung
+ */
+
+package org.example;
+
+import java.util.Scanner;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        Scanner scan = new Scanner(System.in);
+
+        double WItax = .055;
+
+        // Prompt input
+        System.out.println("What is the order amount? ");
+        String Order = scan.nextLine();
+
+        System.out.println("What is the state? ");
+        String State = scan.nextLine();
+
+        double dOrder = Double.parseDouble(Order);
+        double dTotal;
+
+        // If statement for if string is WI and put return at end to stop program
+        if (State.equals("WI"))
+        {
+            double Tax = (dOrder * WItax);
+            dTotal = dOrder + Tax;
+            System.out.println("The subtotal is $"+String.format("%.2f", dOrder)+".\n"+ "The tax is $"+String.format("%.2f", Tax) + ".\n"+ "The total is $"+String.format("%.2f", dTotal)+".");
+            return;
+        }
+
+        // For if state != WI
+        System.out.println("The total is $" +String.format("%.2f", dOrder) + ".");
+    }
+}
